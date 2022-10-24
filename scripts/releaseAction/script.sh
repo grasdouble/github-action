e525ae5697409f7f89c91639e7bbfe35adbddac0
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-24 17:03:20 +0200

    Add releaseAction: Manage release tag for github actions in same repo (#56)

---
+++ scripts/releaseAction/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git fetch --tag
+
+previousCommit=$1
+lastCommit=$2
+
+versions=$(cat ./release.txt)
+
+while read n; do 
+  echo "Name $n"
+  if grep -q $n <<< $(git tag -l); then
+    echo "$n already exist"
+  else
+    git tag $n
+    git push --tag    
+  fi
+done <<< "$versions"
+
