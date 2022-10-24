bd0fa10c4bfd41d5a2d0bbed3483969763061381
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-24 12:31:06 +0000

    try

--- releaseAction/script.sh
+++ releaseAction/script.sh
@@ -1,1 +1,14 @@
-echo "Modified workflows           : $(git diff --name-only ${{ github.event.before }} ${{ github.event.after }})"
+#!/bin/bash
+
+previousCommit=$1
+lastCommit=$2
+
+modifiedFiles=$(git diff --name-only $previousCommit $lastCommit)
+
+i=1;
+
+while read n; do 
+  echo "Name $i. $n"
+  echo "--- $n was found at line $i"
+  i=$(($i+1)); 
+done <<< "$modifiedFiles"
