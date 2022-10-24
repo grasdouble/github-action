bf6e00a7c6c6c46f8160c47311a7aa982cee926d
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-24 14:38:02 +0000

    well done

--- scripts/releaseAction/script.sh
+++ scripts/releaseAction/script.sh
@@ -3,14 +3,15 @@
 previousCommit=$1
 lastCommit=$2
 
-modifiedFiles=$(git diff --name-only $previousCommit $lastCommit)
-
-i=1;
+versions=$(cat ./release.txt)
 
 while read n; do 
-  echo "Name $i. $n"
-  echo "--- $n was found at line $i"
-  readarray -d "/" -t filePath <<< "$n"
-  echo "---- ${filePath[1]}"
-  i=$(($i+1));
-done <<< "$modifiedFiles"
+  echo "Name $n"
+  if grep -q $n <<< $(git tag -l); then
+    echo "$n already exist"
+  else
+    git tag $n
+    git push --tag    
+  fi
+done <<< "$versions"
+
