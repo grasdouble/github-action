ffaf75996d82c9e4b79d8624d2c8915f8dc31ddf
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-24 12:48:02 +0000

    test

--- releaseAction/script.sh
+++ releaseAction/script.sh
@@ -10,7 +10,7 @@ i=1;
 while read n; do 
   echo "Name $i. $n"
   echo "--- $n was found at line $i"
-  readarray -d " " -t filePath <<< "$n"
-  echo "--- $filePath[#strarr[*]-1]"
-  i=$(($i+1)); 
+  readarray -d ".github/workflow/" -t filePath <<< "$n"
+  echo "---- ${filePath}"
+  i=$(($i+1));
 done <<< "$modifiedFiles"
