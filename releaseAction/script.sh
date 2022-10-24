1e1a6ccb3530094b0e66bb79ea0a4ec83ce0349f
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-24 12:37:39 +0000

    test get last element in pathfile

--- releaseAction/script.sh
+++ releaseAction/script.sh
@@ -10,5 +10,7 @@ i=1;
 while read n; do 
   echo "Name $i. $n"
   echo "--- $n was found at line $i"
+  readarray -d " " -t filePath <<< "$n"
+  echo "--- $filePath[#strarr[*]-1]"
   i=$(($i+1)); 
 done <<< "$modifiedFiles"
